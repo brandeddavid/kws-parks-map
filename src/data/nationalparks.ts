c820d34419a3f3ad51c2a024b59cd23f1b2e5220
David Mwangi <david.mathenge98@gmail.com>
2023-12-08 17:39:11 +0300

    add geo data

---
+++ src/data/nationalparks.ts
@@ -0,0 +1,203 @@
+const nationalParks = {
+	type: "FeatureCollection",
+	features: [
+		{
+			geometry: {
+				type: "Point",
+				coordinates: [36.854, -1.354],
+			},
+			type: "Feature",
+			properties: {
+				description: "Nairobi National Park",
+				"marker-symbol": "marker",
+				title: "Nairobi National Park",
+				url: "",
+				lines: ["Green"],
+				address: "Nairobi National Park",
+				"marker-size": "large",
+			},
+		},
+		{
+			geometry: {
+				type: "Point",
+				coordinates: [35.163, -1.482],
+			},
+			type: "Feature",
+			properties: {
+				description: "Masaai Mara National Park",
+				"marker-symbol": "marker",
+				title: "Masaai Mara National Park",
+				url: "",
+				lines: ["Green"],
+				address: "Masaai Mara National Park",
+			},
+		},
+		{
+			geometry: {
+				coordinates: [38.74582663083626, -2.766172962842475],
+				type: "Point",
+			},
+			type: "Feature",
+			properties: {
+				description: "Tsavo East National Park",
+				"marker-symbol": "marker",
+				title: "Tsavo National Park",
+				url: "",
+				lines: ["Green"],
+				address: "Tsavo National Park",
+			},
+		},
+		{
+			geometry: {
+				coordinates: [37.95181838831908, -3.424817255586973],
+				type: "Point",
+			},
+			type: "Feature",
+			properties: {
+				description: "Tsavo West National Park",
+				"marker-symbol": "marker",
+				title: "Tsavo West National Park",
+				url: "",
+				lines: ["Green"],
+				address: "Tsavo West National Park",
+			},
+		},
+		{
+			geometry: {
+				coordinates: [37.241255670017125, -2.638606319900461],
+				type: "Point",
+			},
+			type: "Feature",
+			properties: {
+				description: "Amboseli National Park",
+				"marker-symbol": "marker",
+				title: "Amboseli National Park",
+				url: "",
+				lines: ["Green"],
+				address: "Amboseli National Park",
+			},
+		},
+		{
+			geometry: {
+				coordinates: [39.387778, -4.257222],
+				type: "Point",
+			},
+			type: "Feature",
+			properties: {
+				description: "Shimba Hills National Reserve",
+				"marker-symbol": "marker",
+				title: "Shimba Hills National Reserve",
+				url: "",
+				lines: ["Green"],
+				address: "Shimba Hills National Reserve",
+			},
+		},
+		{
+			geometry: {
+				coordinates: [36.083333, -0.366667],
+				type: "Point",
+			},
+			type: "Feature",
+			properties: {
+				description: "Lake Nakuru National Park",
+				"marker-symbol": "marker",
+				title: "Lake Nakuru National Park",
+				url: "",
+				lines: ["Green"],
+				address: "Lake Nakuru National Park",
+			},
+		},
+		{
+			geometry: {
+				coordinates: [37.53, 0.618056],
+				type: "Point",
+			},
+			type: "Feature",
+			properties: {
+				description: "Samburu National Reserve",
+				"marker-symbol": "marker",
+				title: "Samburu National Reserve",
+				url: "",
+				lines: ["Green"],
+				address: "Samburu National Reserve",
+			},
+		},
+		{
+			geometry: {
+				coordinates: [36.7325, -0.43],
+				type: "Point",
+			},
+			type: "Feature",
+			properties: {
+				description: "Aberdare National Park",
+				"marker-symbol": "marker",
+				title: "Aberdare National Park",
+				url: "",
+				lines: ["Green"],
+				address: "Aberdare National Park",
+			},
+		},
+		{
+			geometry: {
+				coordinates: [39.816667, -3.266667],
+				type: "Point",
+			},
+			type: "Feature",
+			properties: {
+				description: "Arabuko Sokoke National Reserve",
+				"marker-symbol": "marker",
+				title: "Arabuko Sokoke National Reserve",
+				url: "",
+				lines: ["Green"],
+				address: "Arabuko Sokoke National Reserve",
+			},
+		},
+		{
+			geometry: {
+				coordinates: [39.362222, -4.713889],
+				type: "Point",
+			},
+			type: "Feature",
+			properties: {
+				description: "Kisite Mpunguti Marine Park and Reserve",
+				"marker-symbol": "marker",
+				title: "Kisite Mpunguti Marine Park and Reserve",
+				url: "",
+				lines: ["Green"],
+				address: "Kisite Mpunguti Marine Park and Reserve",
+			},
+		},
+		{
+			geometry: {
+				coordinates: [38.208056, 0.093611],
+				type: "Point",
+			},
+			type: "Feature",
+			properties: {
+				description: "Meru National Park",
+				"marker-symbol": "marker",
+				title: "Meru National Park",
+				url: "",
+				lines: ["Green"],
+				address: "Meru National Park",
+			},
+		},
+		{
+			geometry: {
+				coordinates: [37.336667, -0.123889],
+				type: "Point",
+			},
+			type: "Feature",
+			properties: {
+				description: "Mount Kenya National Park",
+				"marker-symbol": "marker",
+				title: "Mount Kenya National Park",
+				url: "",
+				lines: ["Green"],
+				address: "Mount Kenya National Park",
+			},
+		},
+	],
+};
+
+export default nationalParks;
