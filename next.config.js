6ac8c7970aeebce7e5e9d4a3cf3e227f5c0c4fd9
David Mwangi <david.mathenge98@gmail.com>
2023-12-08 17:39:29 +0300

    add map

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+	env: {
+		MAPBOX_TOKEN:
+			"pk.eyJ1IjoiZG13YW5naSIsImEiOiJjbHB1dm15MmwwMGdiMmlwYWUydzltdDVzIn0.Oi1FjbpjMI0noX1-GA2m4Q",
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
